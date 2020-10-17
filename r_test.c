381391581218415bab077f7ecb6257b9ee040e78
Eniac-W <1486791441@qq.com>
2020-10-17 13:22:23 +0800

    r_test

---
+++ r_test.c
@@ -0,0 +1,34 @@
+//#include<stdio.h> 
+//main()
+//{
+//	char sex;
+	//int age;
+	//float height;
+	
+	//scanf("%c",&sex);
+	
+	//scanf("%d",&age);
+	
+///	scanf("%f",&height);	
+	//printf("the sex is %c , the age is %d,and the height is %f",sex,age,height);
+	
+//return 0;The sex is M, the age is 18, and the height is 1.660000.
+
+
+//}
+#include<stdio.h> 
+main()
+{
+	char sex;
+	int age;
+	float height;
+	
+	scanf("%c",&sex);
+	scanf("%d",&age);
+	scanf("%f",&height);	
+	printf("The sex is %c, the age is %d, and the height is %f.",sex,age,height);
+	
+return 0;
+
+
+}
